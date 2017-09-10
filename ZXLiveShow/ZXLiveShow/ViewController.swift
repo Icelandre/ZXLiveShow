626205cfe68c42015efb38fc6b48c6f817e227cb
Icelandre <zhangxiong@zhangxiongdemacbook-pro.local>
2017-09-10 16:50:34 +0800

    初始化项目

---
+++ ZXLiveShow/ZXLiveShow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZXLiveShow
+//
+//  Created by 张雄 on 2017/9/10.
+//  Copyright © 2017年 张雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
